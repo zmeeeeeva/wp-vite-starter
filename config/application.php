23215fd436e526d77f52c3931dbe38a171737fdb
Estr <v@zmeeva.io>
2025-08-26 15:31:35 +0200

    Update roots/wordpress; Update oscarotero/env

--- config/application.php
+++ config/application.php
@@ -18,11 +18,6 @@ $root_dir = dirname(__DIR__);
 $webroot_dir = $root_dir . '/web';
 
 /**
- * Expose global env() function from oscarotero/env
- */
-//Env::init();
-
-/**
  * Use Dotenv to set required environment variables and load .env file in root
  */
 $dotenv = Dotenv\Dotenv::create($root_dir);
